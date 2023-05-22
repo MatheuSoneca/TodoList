de0fa738f58bac21afd392f36642ead545c4e6a3
Matheus Viana Dantas <matheusvianad@hotmail.com>
2023-05-22 20:04:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const buttonCreateTask = document.querySelector("#buttonTask")
+const taskInput = document.querySelector("#taskInput")
+const templateTaskItem = document.querySelector("#taskItem .taskItem")
+const taskList = document.querySelector("#taskList")
+let counter = 0
+
+const removeTask = (id) => {
+  const taskItem = document.querySelector(`[data-id='${id}']`)
+  taskItem.remove()
+}
+
+const createTask = (textInput) => {
+  const newTaskItem = templateTaskItem.cloneNode(true)
+  const textElement = newTaskItem.querySelector("p")
+  const button = newTaskItem.querySelector("button")
+  const id = counter
+
+  newTaskItem.dataset.id = id
+  textElement.innerText = textInput
+  button.onclick = () => removeTask(id)
+  taskList.appendChild(newTaskItem)
+  counter++
+}
+
+buttonCreateTask.onclick = () => {
+  createTask(taskInput.value)
+  taskInput.value = ""
+}
+
